1aa9f4669443aaef0a88c9a653cf7fc8f35b3e52
Ricwol <cedric.r.wolffs@gmail.com>
2025-11-11 19:12:11 +0000

    feat(gui): Add merge button and PDF merging logic
    - Add "Merge PDFs" button to trigger merging
    - Implement merge_pdfs method to combine PDFs
    - Use filedialog to let user choose output location
    - Show success/error message for user feedback

--- src/simplemerge.py
+++ src/simplemerge.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
 
+from pypdf import PdfWriter
 from tkinterdnd2 import TkinterDnD, DND_FILES
 
 
@@ -21,6 +24,15 @@ class SimpleMerge:
         self.drop_area.drop_target_register(DND_FILES)
         self.drop_area.dnd_bind("<<Drop>>", self.on_drop)
 
+        self.merge_button = tk.Button(
+            root,
+            text="Merge PDFs",
+            command=self.merge_pdfs,
+            height=2,
+            width=20
+        )
+        self.merge_button.pack(pady=20)
+
     def on_drop(self, event: tk.Event) -> None:
         files: list[str] = self.root.tk.splitlist(event.data)
         for file in files:
@@ -28,3 +40,26 @@ class SimpleMerge:
             if file_path.suffix.lower() == ".pdf":
                 self.pdf_files.append(file)
                 self.drop_area.insert(tk.END, file_path.name)
+
+    def merge_pdfs(self) -> None:
+        if not self.pdf_files:
+            messagebox.showerror("Error", "No PDFs selected!")
+            return
+        if len(self.pdf_files) == 1:
+            messagebox.showerror("Error", "Can't merge only one PDF file!")
+            return
+
+        output_filename = filedialog.asksaveasfilename(
+            defaultextension=".pdf",
+            filetypes=[("PDF files", "*.pdf")],
+            title="Save merged PDF as..."
+        )
+        if not output_filename:
+            return
+        
+        with PdfWriter() as merger:
+            for pdf in self.pdf_files:
+                merger.append(pdf)
+            merger.write(output_filename)
+        
+        messagebox.showinfo("Success", f"PDFs merged to {output_filename}")
